e0dd1f07d5a0ea3a83ced706f9b9f06403cc7150
dphilipson <dphilipson@palantir.com>
2015-12-28 16:31:09 -0800

    Complete "Storing temporary UI in component state"
    Only real different from tutorial is that with ES6 React components, we
    do not have getInitialState(), so set initial state in the constructor
    instead.

--- App.tsx
+++ App.tsx
@@ -4,15 +4,31 @@ eval(namespaceEvalHack("Todos"));
 
 namespace Todos {
   // App component - represents the whole App
+  interface AppState {
+    hideCompleted: boolean;
+  }
+
   interface AppData {
     tasks: TaskDAO[];
+    incompleteCount: number;
   }
 
   @reactMixin.decorate(ReactMeteorData)
-  export class App extends MeteorComponent<{}, {}, AppData> {
+  export class App extends MeteorComponent<{}, AppState, AppData> {
+    public constructor(props?: {}, context?: any) {
+      super(props, context);
+      this.state = { hideCompleted: false};
+    }
+
     public getMeteorData(): AppData {
+      let query: Mongo.Selector = {};
+      if (this.state.hideCompleted) {
+        // If hide completed is checked, filter tasks
+        query = { checked: { $ne: true } };
+      }
       return {
-        tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch()
+        tasks: Tasks.find(query, { sort: { createdAt: -1 } }).fetch(),
+        incompleteCount: Tasks.find({ checked: { $ne: true } }).count()
       };
     }
 
@@ -40,11 +56,25 @@ namespace Todos {
       inputElement.value = "";
     }
 
+    private toggleHideCompleted(): void {
+      this.setState({
+        hideCompleted: !this.state.hideCompleted
+      });
+    }
+
     public render(): JSX.Element {
       return (
         <div className="container">
           <header>
-            <h1>Todo List</h1>
+            <h1>Todo List ({this.data.incompleteCount})</h1>
+            <label className="hide-completed">
+              <input
+                  type="checkbox"
+                  readOnly={true}
+                  checked={this.state.hideCompleted}
+                  onClick={this.toggleHideCompleted.bind(this)} />
+              Hide Completed Tasks
+            </label>
             <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
               <input
                   type="text"
